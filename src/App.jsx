08495d8db1c6ddecab2f2885f14c414985098b7f
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-23 16:26:38 +0300

    This is the exercises 1.5, course information step5

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,36 @@ import Header from "./components/Header";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = "half Stack application development";
-  const parts = [
-    {
-      name: "fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of component",
-      exercises: 14,
-    },
-  ];
-  const partsNames = parts.map((part, index) => (
+  const course = {
+    name: "half Stack application development",
+    parts: [
+      {
+        name: "fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of component",
+        exercises: 14,
+      },
+    ]
+  }
+  
+  const partsNames = course.parts.map((part, index) => (
     <p key={index}>
       {part.name} {part.exercises}
     </p>
   ));
 
-  const partsExercises = parts.map((part) => part.exercises);
+  const partsExercises = course.parts.map((part) => part.exercises);
   const totalExercises = partsExercises.reduce((acc, val) => acc + val);
 
   return (
     <div>
-      <Header name={course} />
+      <Header name={course.name} />
       <Content partsNames={partsNames} />
       <Total totalExercises={totalExercises} />
     </div>
