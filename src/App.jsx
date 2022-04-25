b7b9299dd2bd0f32a0c7ff99d9059c21a6fc165b
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-25 14:38:21 +0300

    This is the exercise 1.6 - unicafe step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
-import Content from "./components/Content";
-import Header from "./components/Header";
-import Total from "./components/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "half Stack application development",
-    parts: [
-      {
-        name: "fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of component",
-        exercises: 14,
-      },
-    ]
-  }
-  
-  const partsNames = course.parts.map((part, index) => (
-    <p key={index}>
-      {part.name} {part.exercises}
-    </p>
-  ));
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const partsExercises = course.parts.map((part) => part.exercises);
-  const totalExercises = partsExercises.reduce((acc, val) => acc + val);
+  //let's create a new Component called Button to handle the buttons events
+  const handleGoodFeedb = () => setGood(good + 1);
+  const handleNeutralFeedb = () => setNeutral(neutral + 1);
+  const handleBadFeedb = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content partsNames={partsNames} />
-      <Total totalExercises={totalExercises} />
+      <button onClick={handleGoodFeedb}>good</button>
+      <button onClick={handleNeutralFeedb}>neutral</button>
+      <button onClick={handleBadFeedb}>bad</button>
+      <h1>Statistics</h1>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad}
     </div>
   );
 };
