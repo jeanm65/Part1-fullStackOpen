d9c891bf62c870ed6819a23e36c1cb39bce5579b
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-28 10:06:34 +0300

    this is the exercise 1.13 - 1.13*: anecdotes step2

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time',
-    'Any fool can write code that a computer can understand. Good programmers write code that ',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would ref'
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
     ]
       const [selected, setSelected] = useState(0);
-    
+      const updateSelected = () => setSelected(Math.floor(Math.random() * 7));
+      const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
+      const handleAnectodesVotes = () => {
+        const voteCopy = [...votes];
+        voteCopy[selected]+=1;
+        setVotes(voteCopy);
+      }
       return (
         <div>
           {anecdotes[selected]} <br />
-          <button onClick={()=>setSelected(Math.floor(Math.random() * 7))}>Next anecdote</button>
+          <p>has: {votes[selected]} votes</p>
+          <button onClick={handleAnectodesVotes}>vote</button>
+          <button onClick={updateSelected}>Next anecdote</button>
         </div>
       )
 }
