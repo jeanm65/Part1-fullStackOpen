c73ad0e656f862b7cbc3a785e3cbc70ea6c37337
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-28 10:29:13 +0300

    This the exercise 1.14 - anecdotes step3

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,28 @@ const App = () => {
     ]
       const [selected, setSelected] = useState(0);
       const updateSelected = () => setSelected(Math.floor(Math.random() * 7));
+      
+      // votes for the anectodes
       const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
       const handleAnectodesVotes = () => {
         const voteCopy = [...votes];
         voteCopy[selected]+=1;
         setVotes(voteCopy);
       }
+
+      // get the anectode with most votes
+      const max = Math.max(...votes);
+      const displayMostVoted = anecdotes[votes.indexOf(max)];
       return (
         <div>
+          <h1>Anectode of the day</h1>
           {anecdotes[selected]} <br />
           <p>has: {votes[selected]} votes</p>
           <button onClick={handleAnectodesVotes}>vote</button>
           <button onClick={updateSelected}>Next anecdote</button>
+          <h1>Anectodes with most votes</h1>
+          {displayMostVoted}
+          <p>has: {max} votes</p>
         </div>
       )
 }
